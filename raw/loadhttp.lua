5158c28a311cc93fbba75c88606bf8f6dbeca9c2
soul <83529546+soul94@users.noreply.github.com>
2022-02-03 22:26:11 -0600

    Create loadhttp.lua

---
+++ raw/loadhttp.lua
@@ -0,0 +1,22 @@
+loadhttp = function(url)
+    local function fail() return game["Players"].LocalPlayer:kick() end
+    
+    local success, result = pcall(game.HttpGet, game, url)
+    if (not success) then
+        return fail(string.format('Failed to GET url %q for reason: %q', url, tostring(result)))
+    end
+
+    local fn, err = loadstring(result)
+    if (type(fn) ~= 'function') then
+        return fail(string.format('Failed to loadstring url %q for reason: %q', url, tostring(err)))
+    end
+
+    local results = { pcall(fn) }
+    if (not results[1]) then
+        return fail(string.format('Failed to initialize url %q for reason: %q', url, tostring(results[2])))
+    end
+
+    return unpack(results, 2)
+end
+
+return loadhttp
